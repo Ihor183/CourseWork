96b0bb497f512a7add909c20050c934b1fa9656f
Ihor <ihor183@gmail.com>
2017-11-18 21:23:49 +0200

    Replay Tokens

--- LOL/LOL/Source.cpp
+++ LOL/LOL/Source.cpp
@@ -1,7 +1,8 @@
-#include<SFML/Graphics.hpp>
+п»ї#include<SFML/Graphics.hpp>
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<map>
 
 const int SIZE = 40;
 
@@ -13,10 +14,23 @@ struct coordinate {
 	int Wight, Height;
 	int leftPx, topPx;
 	double vec2f1, vec2f2;
+
+	bool operator < (const coordinate &point) const {
+		//if(this->x == point.x && this->y == point.y && this->Wight == point.Wight && this->Height == point.Height)
+		if(Height < point.Height)
+		    return true;
+		return false;
+	}
+
+	bool operator == (const coordinate &point) const {
+		if (this->x == point.x && this->y == point.y && this->Wight == point.Wight && this->Height == point.Height)
+			return true;
+		return false;
+	}
 };
 
 class Map {
-private: 
+private:
 	coordinate SqMap[SIZE];
 
 public:
@@ -61,43 +75,115 @@ Player::Player(string Name) {
 
 class Tokens {
 private:
-	coordinate CoorTok[6];
+	map<coordinate, bool> tokens;
+	map<coordinate, bool>::iterator itokens;
 
 public:
 	friend ifstream& operator >>(ifstream &, Tokens &);
+	void getToken(int*, int*, int*, int*, double*, double*);
 };
 
 ifstream& operator >> (ifstream &ifs, Tokens &obj)
 {
+	coordinate temp;
+	obj.itokens = obj.tokens.begin();
 	for (int i = 0; i < 6; i++) {
-		ifs >> obj.CoorTok[i].x;
-		ifs >> obj.CoorTok[i].y;
-		ifs >> obj.CoorTok[i].Wight;
-		ifs >> obj.CoorTok[i].Height;
-		ifs >> obj.CoorTok[i].leftPx;
-		ifs >> obj.CoorTok[i].topPx;
+		
+		ifs >> temp.x;
+		ifs >> temp.y;
+		ifs >> temp.Wight;
+		ifs >> temp.Height;
+		ifs >> temp.vec2f1;
+		ifs >> temp.vec2f2;
 
+		obj.tokens[temp] = false;
 	}
 
 	return ifs;
 }
 
-void newGame(RenderWindow &window) {
-	Texture tex_Plcol[14];/*, tex_Plcol2, tex_Plcol3, tex_Plcol4*/
-	for (int i = 0; i < 14; i++) {
-		if (i < 4)
-			tex_Plcol[i].loadFromFile("images/color.png");
-		if (i > 3 && i < 8)
-			tex_Plcol[i].loadFromFile("images/Back3.png");
-		if (i > 7)
-			tex_Plcol[i].loadFromFile("images/tokens.tga");
+void Tokens :: getToken(int* a, int* b, int* c, int* d, double* e, double* f) {
+	coordinate temp;
+	temp.x = *a;
+	temp.y = *b;
+	temp.Wight = *c;
+	temp.Height = *d;
+	int i = 0;
+
+	for (itokens = tokens.begin(); itokens != tokens.end(); itokens++) {
+		if ((*itokens).first == temp) {
+			i++;
+			(*itokens).second = false;
+			if ((++itokens) == tokens.end()) {
+				itokens = tokens.begin();
+				
+				(*itokens).second = true;
+				*a = (*itokens).first.x;
+				*b = (*itokens).first.y;
+				*c = (*itokens).first.Wight;
+				*d = (*itokens).first.Height;
+				*e = (*itokens).first.vec2f1;
+				*f = (*itokens).first.vec2f2;
+
+				break;
+			}
+			else {
+				++itokens;
+				(*itokens).second = true;
+				*a = (*itokens).first.x;
+				*b = (*itokens).first.y;
+				*c = (*itokens).first.Wight;
+				*d = (*itokens).first.Height;
+				*e = (*itokens).first.vec2f1;
+				*f = (*itokens).first.vec2f2;
+
+				break;
+			}
+		}
 	}
 
-	Sprite spr_Plcol[14];
-	for (int i = 0; i < 14; i++) 
-			spr_Plcol[i].setTexture(tex_Plcol[i]);
-	
-	for (int i = 0; i < 14; i++) {
+	if (i == 0) {
+	bool flag = false;
+	itokens = tokens.begin();
+	while (!flag && itokens != tokens.end()) {
+		if ((*itokens).second == false) {
+			(*itokens).second = true;
+			*a = (*itokens).first.x;
+			*b = (*itokens).first.y;
+			*c = (*itokens).first.Wight;
+			*d = (*itokens).first.Height;
+			*e = (*itokens).first.vec2f1;
+			*f = (*itokens).first.vec2f2;
+
+			flag = true;
+		}
+		else
+			itokens++;
+	  }
+	}
+}
+
+void newGame(RenderWindow &window) {
+	Texture tex_Plcol[8], tex_Token[4];
+	for (int i = 0; i < 8; i++) {
+		if (i < 4)
+			tex_Plcol[i].loadFromFile("images/color.png");
+		if (i > 3)
+			tex_Plcol[i].loadFromFile("images/Back3.png");
+	}
+
+	for (int i = 0; i < 4; i++) {
+		tex_Token[i].loadFromFile("images/tokens.tga");
+	}
+
+	Sprite spr_Plcol[8], spr_Token[4];
+	for (int i = 0; i < 8; i++)
+		spr_Plcol[i].setTexture(tex_Plcol[i]);
+
+	for (int i = 0; i < 4; i++)
+		spr_Token[i].setTexture(tex_Token[i]);
+
+	for (int i = 0; i < 8; i++) {
 		if (i == 0) {
 			spr_Plcol[i].setTextureRect(IntRect(55, 104, 58, 58));
 			spr_Plcol[i].setPosition(80, 100);
@@ -118,9 +204,9 @@ void newGame(RenderWindow &window) {
 			spr_Plcol[i].setPosition(1228, 200);
 		}
 
-		if (i > 3 && i < 8) {
+		if (i > 3) {
 			spr_Plcol[i].setTextureRect(IntRect(0, 0, 120, 58));
-			
+
 			if (i == 4)
 				spr_Plcol[i].setPosition(138, 100);
 			if (i == 5)
@@ -130,43 +216,37 @@ void newGame(RenderWindow &window) {
 			if (i == 7)
 				spr_Plcol[i].setPosition(1108, 200);
 		}
-
-		if (i == 8) {
-			spr_Plcol[i].setTextureRect(IntRect(74, 65, 103, 106));
-			spr_Plcol[i].setScale(Vector2f(0.56f, 0.547f));
-			spr_Plcol[i].setPosition(80, 100);
-		}
-
-		if (i == 9) {
-			spr_Plcol[i].setTextureRect(IntRect(231, 26, 171, 156));
-			spr_Plcol[i].setScale(Vector2f(0.34f, 0.37f));
-			spr_Plcol[i].setPosition(1228, 100);
-		}
-
-		if (i == 10) {
-			spr_Plcol[i].setTextureRect(IntRect(486, 75, 113,87));
-			spr_Plcol[i].setScale(Vector2f(0.513f, 0.666f));
-			spr_Plcol[i].setPosition(80, 200);
-		}
-
-		if (i == 11) {
-			spr_Plcol[i].setTextureRect(IntRect(30, 195, 140, 123));
-			spr_Plcol[i].setScale(Vector2f(0.414f, 0.47f));
-			spr_Plcol[i].setPosition(1228, 200);
-		}
-
-		if (i == 12) {
-			spr_Plcol[i].setTextureRect(IntRect(251, 237, 123, 74));
-			spr_Plcol[i].setScale(Vector2f(0.47f, 0.783f));
-			//spr_Plcol[i].setPosition(80, 100);
-		}
-
-		if (i == 13) {
-			spr_Plcol[i].setTextureRect(IntRect(480, 225, 155, 81));
-			spr_Plcol[i].setScale(Vector2f(0.374f, 0.716f));
-			//spr_Plcol[i].setPosition(80, 100);
-		}
 	}
+	int a, b, c, d;
+	double e = 0.0, f = 0.0;
+	a = 0, b = 0, c = 1, d = 1;
+	for (int i = 0; i < 4; i++) {
+		spr_Token[i].setTextureRect(IntRect(a, b, c, d));
+		if(i == 0)
+		    spr_Token[i].setPosition(80, 100);
+		if (i == 1) 
+			spr_Token[i].setPosition(1228, 100);
+		if (i == 2) 
+			spr_Token[i].setPosition(80, 200);
+		if (i == 3) 
+			spr_Token[i].setPosition(1228, 200);
+		
+	}
+		
+	
+	Tokens tokens;
+	ifstream ifs;
+	ifs.open("Tokens.txt", ios::in);
+
+	if (!ifs)
+	{
+		cout << "Error, file is not founded\n";
+		return;
+	}
+
+	ifs >> tokens;
+
+	ifs.close();
 
 	Texture menuTexture4, menuBackground;
 	menuTexture4.loadFromFile("images/4.png");
@@ -178,13 +258,13 @@ void newGame(RenderWindow &window) {
 	menu2.setPosition(80, 360);
 	menuBg.setPosition(0, 0);
 
-	string Play = "Play", NotPlay = "Not Selecting" ;
+	string Play = "Play", NotPlay = "Not Selecting";
 
 	Font fnt_Arial;
 	fnt_Arial.loadFromFile("fonts/arial.ttf");
 	Text text1("", fnt_Arial, 14), text2("", fnt_Arial, 14), text3("", fnt_Arial, 14), text4("", fnt_Arial, 14);
-	
-	
+
+
 	//text.setColor(Color::White);
 	text1.setStyle(Text::Bold);
 	text1.setString(NotPlay);
@@ -202,109 +282,75 @@ void newGame(RenderWindow &window) {
 	text4.setString(NotPlay);
 	text4.setPosition(1110, 220);
 
-	int i = 0;
+	//int i = 0;
 
 	while (isMenu) {
 		menuNum = 0;
 		window.clear();
 
-		if (IntRect(80, 100, 58, 58).contains(Mouse::getPosition(window))) 
+		if (IntRect(80, 100, 58, 58).contains(Mouse::getPosition(window)))
 			menuNum = 1;
-		if	(IntRect(1228, 100, 58, 58).contains(Mouse::getPosition(window)))
+		if (IntRect(1228, 100, 58, 58).contains(Mouse::getPosition(window)))
 			menuNum = 2;
-		if	(IntRect(80, 200, 58, 58).contains(Mouse::getPosition(window)))
+		if (IntRect(80, 200, 58, 58).contains(Mouse::getPosition(window)))
 			menuNum = 3;
-		if	(IntRect(1228, 200, 58, 58).contains(Mouse::getPosition(window)))
+		if (IntRect(1228, 200, 58, 58).contains(Mouse::getPosition(window)))
 			menuNum = 4;
-		
-		if (Mouse::isButtonPressed(Mouse::Left)) {
-			if (menuNum == 1) {
-				i++;
-				if (i == 6)
-					i = 0;
-				if()
+		if (IntRect(80, 360, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 5; }
 
+		if (Mouse::isButtonPressed(Mouse::Left) || menuNum == 1) {
+			if (menuNum == 1) {
+				tokens.getToken(&a, &b, &c, &d, &e, &f);
+				spr_Token[0].setTextureRect(IntRect(a, b, c, d));
+				spr_Token[0].setScale(Vector2f(e, f));
+				spr_Token[0].setPosition(80, 100);
+				sleep(*new Time(milliseconds(200)));
 			}
 
 			if (menuNum == 2) {
-
+				tokens.getToken(&a, &b, &c, &d, &e, &f);
+				spr_Token[1].setTextureRect(IntRect(a, b, c, d));
+				spr_Token[1].setScale(Vector2f(e, f));
+				spr_Token[1].setPosition(1228, 100);
+				sleep(*new Time(milliseconds(200)));
 			}
 
 			if (menuNum == 3) {
-
+				tokens.getToken(&a, &b, &c, &d, &e, &f);
+				spr_Token[2].setTextureRect(IntRect(a, b, c, d));
+				spr_Token[2].setScale(Vector2f(e, f));
+				spr_Token[2].setPosition(80, 200);
+				sleep(*new Time(milliseconds(200)));
 			}
 
 			if (menuNum == 4) {
-
+				tokens.getToken(&a, &b, &c, &d, &e, &f);
+				spr_Token[3].setTextureRect(IntRect(a, b, c, d));
+				spr_Token[3].setScale(Vector2f(e, f));
+				spr_Token[3].setPosition(1228, 200);
+				sleep(*new Time(milliseconds(200)));
 			}
+
+			if (menuNum == 5) { window.close(); isMenu = false; }
 		}
 
 		window.draw(menuBg);
 		window.draw(menu2);
 
-		for(int i = 0; i < 12; i++)
-		window.draw(spr_Plcol[i]);
+		for (int i = 0; i < 8; i++)
+			window.draw(spr_Plcol[i]);
+
+		for (int i = 0; i < 4; i++)
+			window.draw(spr_Token[i]);
 
 		window.draw(text1);
 		window.draw(text2);
 		window.draw(text3);
 		window.draw(text4);
-		
+
 		window.display();
 
 	}
-
-	
-	
-	//Texture menuTexture1, menuTexture4, menuBackground;
-	//menuTexture1.loadFromFile("images/1color.png");
-	//menuTexture4.loadFromFile("images/4.png");
-	//menuBackground.loadFromFile("images/back1.png");
-	//Sprite menu1, menu2(menuTexture4), menuBg(menuBackground);
-	//menu1.setTexture(menuTexture1);
-	//bool isMenu = 1;
-	//int menuNum = 0, i = 0;
-
-	//menu1.setTextureRect(IntRect(55, 104, 58, 58));
-	//menu1.setPosition(80, 100);
-	//menu2.setPosition(80, 360);
-	//menuBg.setPosition(0, 0);
-	////////////////////////////////МЕНЮ///////////////////
-	//menu1.setColor(Color::Blue);
-	//while (isMenu)
-	//{
-	//	menu2.setColor(Color::White);
-	//	menuNum = 0;
-	//	window.clear();
-	//	if (IntRect(80, 100, 58, 58).contains(Mouse::getPosition(window))) {
-	//		menuNum = 1;
-	//	}
-	//	if (IntRect(80, 360, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
-	//	if (Mouse::isButtonPressed(Mouse::Left))
-	//	{
-	//		if (menuNum == 1) {
-	//			i++;
-	//			if (i > 3)
-	//				i = 0;
-	//			if (i == 0)
-	//				menu1.setColor(Color::Blue);
-	//			if (i == 1)
-	//				menu1.setColor(Color::Red);
-	//			if (i == 2)
-	//				menu1.setColor(Color::Yellow);
-	//			if (i == 3)
-	//				menu1.setColor(Color::Green);
-	//			
-	//			sleep(*new Time(milliseconds(200)));
-
-	//		}//если нажали первую кнопку, то выходим из меню 
-	//		if (menuNum == 2) { window.close(); isMenu = false; }
-	//	}
-	//	window.draw(menuBg);
-	//	window.draw(menu1);
-	//	window.draw(menu2);
-	//	window.display();
-	//}
 }
 
 void menu(RenderWindow &window) {
@@ -321,7 +367,7 @@ void menu(RenderWindow &window) {
 	menu3.setPosition(80, 420);
 	menuBg.setPosition(0, 0);
 	menuBg.setScale(Vector2f(1.7f, 1.225f));
-	//////////////////////////////МЕНЮ///////////////////
+
 	while (isMenu)
 	{
 		menu1.setColor(Color::White);
@@ -332,11 +378,12 @@ void menu(RenderWindow &window) {
 		if (IntRect(80, 300, 300, 50).contains(Mouse::getPosition(window))) { menu1.setColor(Color::Blue); menuNum = 1; }
 		if (IntRect(80, 360, 300, 50).contains(Mouse::getPosition(window))) { menu2.setColor(Color::Blue); menuNum = 2; }
 		if (IntRect(80, 420, 300, 50).contains(Mouse::getPosition(window))) { menu3.setColor(Color::Blue); menuNum = 3; }
-		if (Mouse::isButtonPressed(Mouse::Left))
+		if (Mouse::isButtonPressed(Mouse::Left) || menuNum == 1)
 		{
-			if (menuNum == 1) { 
+			if (menuNum == 1) {
 				newGame(window);
-				isMenu = false; }//если нажали первую кнопку, то выходим из меню 
+				isMenu = false;
+			}
 			if (menuNum == 2) { window.draw(menu2); window.display(); while (!Keyboard::isKeyPressed(Keyboard::Escape)); }
 			if (menuNum == 3) { window.close(); isMenu = false; }
 		}
@@ -349,7 +396,7 @@ void menu(RenderWindow &window) {
 }
 
 int main() {
-	RenderWindow App; 
+	RenderWindow App;
 	App.create(VideoMode::getDesktopMode(), "Monopoly");
 	menu(App);
 
@@ -360,7 +407,7 @@ int main() {
 	}
 	Text text("", fnt_Arial, 20);
 	text.setStyle(Text::Bold);
-	
+
 
 	Texture txt_map;
 	if (!txt_map.loadFromFile("images/map.png"))
@@ -386,7 +433,7 @@ int main() {
 	spr_tokens.setTexture(tex_tokens);
 	spr_backgr.setTexture(tex_background);
 	spr_backgr.setPosition(0, 0);
-	
+
 
 	ifstream ifs;
 	Map map;
@@ -419,21 +466,22 @@ int main() {
 
 		App.clear();
 		App.draw(spr_backgr);
-			App.draw(spr_map);
-			spr_tokens.setTextureRect(IntRect(227, 18, 183, 167));
-			spr_tokens.setScale(Vector2f(0.3f, 0.3f));
-			spr_tokens.setPosition(990, 585);
-			App.draw(spr_tokens);
+		App.draw(spr_map);
+		spr_tokens.setTextureRect(IntRect(227, 18, 183, 167));
+		spr_tokens.setScale(Vector2f(0.3f, 0.3f));
+		spr_tokens.setPosition(990, 585);
+		App.draw(spr_tokens);
 
-			spr_tokens.setTextureRect(IntRect(44, 44, 149, 149));
-			spr_tokens.setScale(Vector2f(0.3f, 0.3f));
-			spr_tokens.setPosition(990, 610);
-			App.draw(spr_tokens);
-		//}
+		spr_tokens.setTextureRect(IntRect(44, 44, 149, 149));
+		spr_tokens.setScale(Vector2f(0.3f, 0.3f));
+		spr_tokens.setPosition(990, 610);
+		App.draw(spr_tokens);
 		
-		
+
+
 		App.display();
 	}
 
 	return 0;
 }
+
