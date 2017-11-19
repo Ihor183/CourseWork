152e0a7d9ce305446a3625122af83a05421fc106
Ihor <ihor183@gmail.com>
2017-11-19 22:52:30 +0200

    Some Finish Menu

---
+++ LOL/LOL/Map.cpp
@@ -0,0 +1,20 @@
+#include "Map.h"
+
+Map::Map() { }
+
+
+Map::~Map() { }
+
+ifstream& operator >> (ifstream &ifs, Map &obj)
+{
+	for (int i = 0; i < SIZE; i++) {
+		ifs >> obj.SqMap[i].x;
+		ifs >> obj.SqMap[i].y;
+		ifs >> obj.SqMap[i].Wight;
+		ifs >> obj.SqMap[i].Height;
+		ifs >> obj.SqMap[i].leftPx;
+		ifs >> obj.SqMap[i].topPx;
+	}
+
+	return ifs;
+}
